0f4510854f2b60e5480b352d748b6947ef96bbfb
MiguelANunes <miguel.alfredo.nunes@gmail.com>
2021-04-10 15:53:17 -0300

    Revert "Especie Feito"
    This reverts commit f6d8e545935bcf1f72f352b931609fc986b45e5d, reversing
    changes made to 71d213c02bd0de795dab8c6098e82419605cea13.

--- BAN1/src/ban1/EspecieController.java
+++ BAN1/src/ban1/EspecieController.java
@@ -1,34 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ban1;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
-
+/**
+ *
+ * @author Pichau
+ */
 public class EspecieController {
     
-    public void createEspecie(Connection C) throws SQLException{
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Insira as Informações da Espécie:");
-        System.out.print("Código: ");
-        int codE = sc.nextInt();
-        
-        System.out.println("");
-        System.out.print("Nome: ");
-        String nome = sc.nextLine();
-        
-        System.out.println("");
-        System.out.print("Expectativa de Vida: ");
-        int expectativa = sc.nextInt();
-        
-        EspecieBean EB = new EspecieBean(codE, expectativa, nome);
-        // EspecieModel.create(EB,C);
-        System.out.println("Especie Adicionada com Sucesso !");
-    }
-    
-    public void ListarEspecies(){
-        // Precisa do Model
-    }
 }
