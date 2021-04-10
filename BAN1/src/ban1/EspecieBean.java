47aea38900bc37ec14e3701b5d23c83bb9e983f9
MiguelANunes <miguel.alfredo.nunes@gmail.com>
2021-04-10 15:49:59 -0300

    EspecieController feito

--- BAN1/src/ban1/EspecieBean.java
+++ BAN1/src/ban1/EspecieBean.java
@@ -4,5 +4,58 @@ public class EspecieBean {
     private int codespecie;
     private int expectativaespecie;
     private String nomeespecie;
+
+    public EspecieBean(int codespecie, int expectativaespecie, String nomeespecie) {
+        this.codespecie = codespecie;
+        this.expectativaespecie = expectativaespecie;
+        this.nomeespecie = nomeespecie;
+    }
+
+    public int getCodespecie() {
+        return codespecie;
+    }
+
+    public int getExpectativaespecie() {
+        return expectativaespecie;
+    }
+
+    public String getNomeespecie() {
+        return nomeespecie;
+    }
+
+    public void setCodespecie(int codespecie) {
+        this.codespecie = codespecie;
+    }
+
+    public void setExpectativaespecie(int expectativaespecie) {
+        this.expectativaespecie = expectativaespecie;
+    }
+
+    public void setNomeespecie(String nomeespecie) {
+        this.nomeespecie = nomeespecie;
+    }
+
+    @Override
+    public String toString() {
+        return "EspecieBean{" + "codespecie=" + codespecie + ", expectativaespecie=" + expectativaespecie + ", nomeespecie=" + nomeespecie + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EspecieBean other = (EspecieBean) obj;
+        if (this.codespecie != other.codespecie) {
+            return false;
+        }
+        return true;
+    }
 }
 
